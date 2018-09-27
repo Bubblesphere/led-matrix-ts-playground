e3375513d6f8e88ce4c2bad110e9fcd03b97ee38
Déric Dallaire <dallaire.deric@gmail.com>
2018-09-27 11:42:31 -0400

    Init

---
+++ src/Profile/Profile.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Profile extends Component {
+  render() {
+    return (
+      <div className="Profile">
+        <h1>Profile 1</h1>
+        <input type="checkbox" value="Autosave" />
+        <hr/>
+      </div>
+    );
+  }
+}
+
+export default Profile;
