8288cc0a1c4843f9a9bcb5688db449b37540add8
Déric Dallaire <dallaire.deric@gmail.com>
2018-10-01 15:29:37 -0400

    Added Select and modified for lastCapturedValue

--- src/Profile/Profile.tsx
+++ src/Profile/Profile.tsx
@@ -2,11 +2,16 @@ import * as React from 'react';
 import { Component, ReactNode } from 'react';
 import TooltipSlider from '../Inputs/TooltipSlider';
 import ProfileFormItem from './ProfileFormItem';
-import { Grid, Typography, Checkbox } from '@material-ui/core';
+import { Grid, Typography, Checkbox, Select, MenuItem } from '@material-ui/core';
+import { ToggleButton, ToggleButtonGroup } from '@material-ui/lab';
 import { StyleSheet, css } from 'aphrodite';
 
 interface ProfileState {
-  increment: number
+  panel: number,
+  increment: number,
+  fps: number,
+  width: number,
+  spacing: number
 }
 
 interface ProfileProps {
@@ -20,6 +25,7 @@ const styles = StyleSheet.create({
 
 class Profile extends Component<ProfileProps, ProfileState> {
   state = {
+    panel: 0,
     increment: 1,
     fps: 60,
     width: 80,
@@ -28,24 +34,45 @@ class Profile extends Component<ProfileProps, ProfileState> {
 
   constructor(props) {
     super(props);
+    this.handleChangesSelect = this.handleChangesSelect.bind(this);
     this.handleChanges = this.handleChanges.bind(this);
   }
 
-  private handleChanges(id, value) {
-    this.setState((prevState) => ({ ...prevState, [id]: value }));
+  handleChangesSelect(event) {
+    this.setState((prevState) => ({ ...prevState, increment: 6 }));
+    //this.handleChanges(event.target.name, event.target.value);
   }
+
+  handleChanges(property, value) {
+    console.log(`${property}: ${value}`);
+    this.setState((prevState) => ({ ...prevState, [property]: value }));
+  }
+
+
 
   render() {
     return (
       <Grid item={true} xs={3} container={true} className={css(styles.profiles)} direction={"column"}>
     
+        <ProfileFormItem name="panel">
+
+          <Select
+            name="panel"
+            value={this.state.panel}
+            onChange={this.handleChangesSelect}
+          >
+            <MenuItem value={0}>Side scrolling</MenuItem>
+            <MenuItem value={1}>Vertical scrolling</MenuItem>
+          </Select>
+        </ProfileFormItem>
+
         <ProfileFormItem name="Increment">
           <TooltipSlider 
             id="increment"
             min={0} 
             max={20} 
-            value={this.state.increment} 
-            onChange={this.handleChanges} 
+            lastCapturedValue={this.state.increment} 
+            onChangeCapture={this.handleChanges} 
           />
         </ProfileFormItem>
 
@@ -54,8 +81,8 @@ class Profile extends Component<ProfileProps, ProfileState> {
             id="fps"
             min={0} 
             max={60} 
-            value={this.state.fps} 
-            onChange={this.handleChanges} 
+            lastCapturedValue={this.state.fps} 
+            onChangeCapture={this.handleChanges} 
           />
         </ProfileFormItem>
 
@@ -64,8 +91,8 @@ class Profile extends Component<ProfileProps, ProfileState> {
             id="width"
             min={1} 
             max={200} 
-            value={this.state.width} 
-            onChange={this.handleChanges} 
+            lastCapturedValue={this.state.width} 
+            onChangeCapture={this.handleChanges} 
           />
         </ProfileFormItem>
 
@@ -74,8 +101,8 @@ class Profile extends Component<ProfileProps, ProfileState> {
             id="spacing"
             min={0} 
             max={20} 
-            value={this.state.spacing} 
-            onChange={this.handleChanges} 
+            lastCapturedValue={this.state.spacing} 
+            onChangeCapture={this.handleChanges} 
           />
         </ProfileFormItem>
 
